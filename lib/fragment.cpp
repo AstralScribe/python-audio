5b2c44491dc336966f30a981cb86d153ba7e3d93
Mayank Sinha <sinha.mayank900@gmail.com>
2024-10-23 13:11:36 +0530

    AudioFragment class for reading audio files

--- lib/fragment.cpp
+++ lib/fragment.cpp
@@ -1,58 +1,106 @@
 #include "fragment.h"
 
-#include <pybind11/pybind11.h>
-
-#include <cstdint>
+#include <fstream>
+#include <iostream>
 
 #include "audioop.h"
 
 namespace fragment {
-AudioFragment::AudioFragment(py::buffer *buffer) : buffer(buffer) {
-  py::buffer_info buf = buffer->request();
-  py::size_t size = buf.size;
+
+AudioFragment::AudioFragment(std::string &filename) {
+  std::ifstream file(filename);
+  file >> std::noskipws;
+  std::vector<unsigned char> audio_vector(
+      (std::istream_iterator<unsigned char>(file)),
+      (std::istream_iterator<unsigned char>()));
+
+  unsigned char *audio_file = audio_vector.data();
+  std::string header;
+  header.assign(reinterpret_cast<char *>(audio_file), 4);
+  if (header != "RIFF")
+    std::runtime_error("Only wav file is currently supported");
+
+  // int32_t file_size = *reinterpret_cast<int32_t *>(audio_file + 4);
+  int16_t format = *reinterpret_cast<int16_t *>(audio_file + 20);
+  channels = *reinterpret_cast<int16_t *>(audio_file + 22);
+  width = 2;
+  sampling_rate = *reinterpret_cast<int32_t *>(audio_file + 24);
+  size = audio_vector.size() - 44;
+
+  std::cout << audio_vector.size() << std::endl;
+
+  if (format == 1) {
+    buffer = audio_file + 44;
+  } else {
+    throw std::runtime_error("Only PCM is currently supported");
+  }
 }
-AudioFragment::AudioFragment(py::buffer *buffer, int sampling_rate, int width,
+
+AudioFragment::AudioFragment(py::buffer *fragment) {
+  py::buffer_info buf = fragment->request();
+  py::size_t size = buf.size;
+  unsigned char *audio_file = static_cast<unsigned char *>(buf.ptr);
+
+  std::string header;
+  header.assign(reinterpret_cast<char *>(audio_file), 4);
+  if (header != "RIFF")
+    std::runtime_error("Only wav file is currently supported");
+
+  // int32_t file_size = *reinterpret_cast<int32_t *>(audio_file + 4);
+  int16_t format = *reinterpret_cast<int16_t *>(audio_file + 20);
+  channels = *reinterpret_cast<int16_t *>(audio_file + 22);
+  width = 2;
+  sampling_rate = *reinterpret_cast<int32_t *>(audio_file + 24);
+
+  std::cout << "Format: " << format << std::endl;
+
+  if (format == 1) {
+    buffer = audio_file + 44;
+  } else {
+    throw std::runtime_error("Only PCM is currently supported");
+  }
+}
+
+AudioFragment::AudioFragment(py::buffer *fragment, int sampling_rate, int width,
                              int channels)
-    : buffer(buffer),
+    : buffer(static_cast<unsigned char *>(fragment->request().ptr)),
+      size(fragment->request().size),
       sampling_rate(sampling_rate),
       width(width),
       channels(channels) {
-  py::buffer_info buf = buffer->request();
-  py::size_t size = buf.size;
-
-  audioop_check_size(width);
-  audioop_check_parameters(size, width);
-
-  int array_size = size / width;
-  array.reserve(array_size);
-
-  for (int i = 0; i < array_size; i += width) {
-    array.push_back(GETINT16(buf.ptr, i));
-  }
+  check_size(width);
+  check_parameters(size, width);
 }
-std::vector<int16_t> AudioFragment::get_array_of_fragment() { return array; }
+AudioFragment::~AudioFragment() {}
+
+py::array AudioFragment::get_array_of_fragment() {
+  std::vector<int16_t> audio_array;
+  int array_size = size / width;
+  audio_array.reserve(array_size);
+
+  for (auto i = 0; i < size; i += width) {
+    audio_array.push_back(buffer[i + 1] << 8 | buffer[i]);
+  }
+
+  return py::array(py::dtype::of<int16_t>(), {audio_array.size()},
+                   {sizeof(int16_t)}, audio_array.data());
+}
+
 int AudioFragment::get_sampling_rate() { return sampling_rate; }
 int AudioFragment::get_width() { return width; }
 int AudioFragment::get_channels() { return channels; }
 
 }  // namespace fragment
 
-class Pet {
- public:
-  Pet(const std::string &name) : name(name) {}
-  void setName(const std::string &name_) { name = name_; }
-  const std::string &getName() const { return name; }
-
- private:
-  std::string name;
-};
-// py::class_<Pet>(m, "Pet")
-//     .def(py::init<const std::string &>())
-//     .def_property
-//     ("name", &Pet::getName, &Pet::setName)
-//
-// PYBIND11_MODULE(_fragment, m) {
-//   py::class_<fragment::AudioFragment>(m, "AudioFragment")
-//     .def(py::init<py::buffer *, int, int, int>())
-//     .def()
-// }
+void _init_submodule_fragment(pybind11::module_ &m) {
+  auto m_a = m.def_submodule("fragment", "Manages Audio.");
+  py::class_<fragment::AudioFragment>(m_a, "AudioFragment")
+      .def(py::init<std::string &>())
+      .def(py::init<py::buffer *>())
+      .def(py::init<py::buffer *, int, int, int>())
+      .def("get_array_of_fragment",
+           &fragment::AudioFragment::get_array_of_fragment, "")
+      .def("get_sampling_rate", &fragment::AudioFragment::get_sampling_rate, "")
+      .def("get_width", &fragment::AudioFragment::get_width, "")
+      .def("get_channels", &fragment::AudioFragment::get_channels, "");
+}
